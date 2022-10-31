d50c64ba6e52126bc22e0a3bdb7120512ab4b47b
Gaston Gamietea <ggamietea@gmail.com>
2022-10-31 17:10:14 -0300

    adding missing karma config

--- karma.conf.js
+++ karma.conf.js
@@ -33,12 +33,18 @@ module.exports = function (config)
                 {type: 'text-summary'}
             ]
         },
+        customLaunchers: {
+          ChromeHeadlessNoSandbox: {
+            base: "ChromeHeadless",
+            flags: ["--no-sandbox"]
+          }
+        },
         reporters          : ['progress', 'kjhtml'],
         port               : 9876,
         colors             : true,
         logLevel           : config.LOG_INFO,
         autoWatch          : true,
-        browsers           : ['Chrome'],
+        browsers           : ['ChromeHeadless'],
         singleRun          : false,
         restartOnFileChange: true
     });
