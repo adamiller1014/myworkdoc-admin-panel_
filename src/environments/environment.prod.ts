6ef938666506880781af8b10e189b42f3d963c4e
A.Miller <adamiller1014@gmail.com>
2024-03-01 06:46:29 -0500

    update endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  restAPI: 'https://mwd-api.selectenergy.com/api/v4/',
+  restAPI: 'https://apiprod.myworkdoc.com/api/v4/',
   version: '4.0.0'
 };
